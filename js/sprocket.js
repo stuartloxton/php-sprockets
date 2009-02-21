2d09a84b6a228ac5e856cd589f837e64f051b5a4
Stuart Loxton <stuart.loxton@googlemail.com>
2009-02-21 17:17:55 +0000

    Caching is now working, also appending ?debug to the JS files URI will stop caching taking place, perfect for development

--- js/sprocket.js
+++ js/sprocket.js
@@ -1,5 +1,7 @@
 $(function() {
 	console.log('Test');
+	// Single line comments will be stripped out by Sprocket
+	/* However multiline ones will stay */
 	//= require "formCancel"
 	//= require <formCancel2>
 });
--- lib/phpsprocket.php
+++ lib/phpsprocket.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: text/plain');
+header('Content-type: application/x-javascript');
 /**
 * PHPSprocket - A PHP implementation of Sprocket
 */
@@ -7,15 +7,17 @@ class PHPSprocket
 {
 	
 	var $baseUri = '/phpsprocket';
-	var $baseJs = '../';
+	var $baseJs = '../js';
 	var $js = '';
 	var $filePath = '';
 	
 	function __construct($file) {
 		$this->filePath = str_replace($this->baseUri, '..', $file);
-		$this->checkCached();
-		$js = $this->parseJS(basename($this->filePath), dirname($this->filePath));
-		echo $js;
+		if(!isset($_GET['debug'])) $this->checkCached();
+		$this->js = $this->parseJS(basename($this->filePath), dirname($this->filePath));
+		$this->stripComments();
+		echo $this->js;
+		if(!isset($_GET['debug'])) file_put_contents($this->filePath.'.cache', $this->js);
 	}
 	
 	function checkCached() {
@@ -38,14 +40,17 @@ class PHPSprocket
 		return $js;
 	}
 	
+	function stripComments() {
+		$this->js = preg_replace('/\/\/([^\n]+)/', '', $this->js);
+	}
+	
 	function require_command($param, $context) {
 		if(preg_match('/\"([^\"]+)\"/', $param, $match)) {
 			return $this->parseJS(basename($context.'/'.$match[1].'.js'), dirname($context.'/'.$match[1].'.js'));
 		} else if(preg_match('/\<([^\>]+)\>/', $param, $match)) {
-			return $this->parseJS(basename($context.'/'.$match[1].'.js'), $this->baseJS);
+			return $this->parseJS(basename($context.'/'.$match[1].'.js'), $this->baseJs);
 		} else return '';
 	}
 	
 }
-
-$sprocket = new PHPSprocket($_SERVER['REQUEST_URI']);
+$sprocket = new PHPSprocket(preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']));
